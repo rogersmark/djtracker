1a56e4b8cdae32fc12b6e6a4331373ffb204dde9
f4nt <f4nt@f4ntasmic.com>
2009-11-05 17:55:37 -0600

    initial project setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "djtracker",
+    version = "0.1",
+    author = "Mark Rogers",
+    author_email = "f4nt@f4ntasmic.com",
+    url = "http://www.f4ntasmic.com",
+
+    packages = find_packages('.'),
+    package_dir = {'':'.'},
+    license = "BSD License",
+    keywords = "django issue tracker",
+    description = "An issue tracker built with django",
+    install_requires=[
+        '',
+    ],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        'Programming Language :: Python',
+    ]
+)
+
