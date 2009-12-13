6ec796ca886e086fb5a8d7a4bec3f0ac6af72c16
chris <chris@baldur.secept.corp>
2009-12-14 00:42:42 +0100

    * add notification when issue is created or updated * add MailTemplate class which allows to keep mail subject and body (and optionally HTML body) in one template * add some test data and unittest for notification stuff
    Note: after applying this patch, there may be exceptions when loading initial_data (typically in execution of unittest). This is caused by initial_data referencing users which are not there yet and can safely be ignored.

--- djtracker/__init__.py
+++ djtracker/__init__.py
@@ -1,6 +1,6 @@
 import datetime
 
-from djtracker import models
+from djtracker import models, utils
 
 from django.contrib.auth.models import User
 from django.contrib.comments.models import Comment
@@ -8,45 +8,63 @@ from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.contrib.sites.models import Site
 from django.conf import settings
-from django.template.loader import get_template
 from django.template import Context
 
-def create_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):    
     if created:
         profile = models.UserProfile()
         profile.user = instance
         profile.save()
 
-def update_watchers(sender, instance, created, **kwargs):
-    comment = instance
+def on_postsave_issue(sender, instance, created, **kwargs):
+    if not hasattr(instance, '_dont_notify_watchers'):
+        update_watchers(issue=instance, created=created)
+
+def on_postsave_comment(sender, instance, created, **kwargs):
+    update_watchers(issue=instance.content_object, comment=instance, created=created)
+
+def update_watchers(issue, created, comment=None):    
     site = Site.objects.get(id=settings.SITE_ID)
-    if comment.content_type.name == "issue":
-        issue = comment.content_object
-        # we send email to: all watchers, the creator and the current processor...
-        email_addresses = list(issue.watched_by.all().values_list('user__email', flat=True))
-        if issue.created_by:
-            email_addresses.append(issue.created_by.user.email)
-        if issue.assigned_to: 
-            email_addresses.append(issue.assigned_to.user.email)
-        # make list unique
-        email_addresses = {}.fromkeys(email_addresses).keys()
-        email_title = "DjTracker: [%s]: Issue #%s has been updated by %s" % (comment.content_object.project.slug, 
-            comment.content_object.id, comment.user_name)
-        t = get_template('djtracker/mail/issue_updated.txt')
-        email_body = t.render(Context({'comment': comment, 'site': site}))
-        # send mails seperately to protect privacy
-        for recipient in email_addresses:
-            send_mail(email_title, email_body, settings.ISSUE_ADDRESS, [recipient], fail_silently=False)
+    context = Context({'issue': issue, 'project': issue.project, 'site': site})
+    
+    if comment:
+        # issue commented
+        context['comment'] = comment
+        context['user_name'] = comment.user_name
+        template = utils.MailTemplate('djtracker/mail/issue_commented.mail')
+    elif created:
+        template = utils.MailTemplate('djtracker/mail/issue_created.mail')        
+    else:
+        template = utils.MailTemplate('djtracker/mail/issue_updated.mail')
+        
+    # we send email to: all watchers, the creator and the current processor...
+    email_addresses = list(issue.watched_by.all().values_list('user__email', flat=True))
+    if issue.created_by:
+        email_addresses.append(issue.created_by.user.email)
+    if issue.assigned_to: 
+        email_addresses.append(issue.assigned_to.user.email)
+    # make list unique
+    email_addresses = {}.fromkeys(email_addresses).keys()
+    
+    # send mails seperately to protect privacy
+    for recipient in email_addresses:
+        msg = template.render_to_mail(context)
+        msg.from_address = settings.ISSUE_ADDRESS
+        msg.to = [recipient,]
+        msg.send()
 
 def update_modified_time(sender, instance, created, **kwargs):
     comment = instance
     if comment.content_type.name == "issue":
         issue = comment.content_object
         issue.modified_date = datetime.datetime.now()
+        # hack to prevent additional mail when new comment is added
+        issue._dont_notify_watchers = True
         issue.save()
 
 post_save.connect(create_profile, sender=User)
-post_save.connect(update_watchers, sender=Comment)
+post_save.connect(on_postsave_comment, sender=Comment)
+post_save.connect(on_postsave_issue, sender=models.Issue)
 post_save.connect(update_modified_time, sender=Comment)
 
 if not hasattr(settings, "WEB_SERVER"):
