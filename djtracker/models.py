1210effb2c0536358f38471429b757b7f4b1ddbb
Patrick Lauber <patrick.lauber@divio.ch>
2009-12-21 22:09:20 +0100

    made models i18n ready

--- djtracker/models.py
+++ djtracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from djtracker.utils import check_perms
 
@@ -9,13 +10,15 @@ class UserProfile(models.Model):
     Extension of User to allow for more detailed information about a
     user
     """
-    instant_messanger = models.CharField(max_length=256,
+    instant_messanger = models.CharField(
+        _("instant messanger"), 
+        max_length=256,
         blank=True,
         null=True
     )
-    user = models.OneToOneField(User, unique=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(User, unique=True, verbose_name=_("user"))
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.user.username
@@ -65,82 +68,102 @@ class Project(models.Model):
     forth. Consider it a grouping mechanism for all the moving parts.
     """
     ## Project descriptions
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(blank=True, null=True)
-    git_repo_path = models.CharField(max_length=1024,
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
+    description = models.TextField(_("description"), blank=True, null=True)
+    git_repo_path = models.CharField(
+        _("git repo path"),
+        max_length=1024,
         blank=True,
         null=True
     )
-    git_repo_commit = models.TextField(max_length=512,
+    git_repo_commit = models.TextField(
+        _("git repo commit"),
+        max_length=512,
         blank=True,
         null=True
     )
-    svn_repo_url = models.CharField(max_length=1024,
+    svn_repo_url = models.CharField(
+        _("svn repo url"),
+        max_length=1024,
         blank=True,
         null=True
     )
-    svn_repo_commit = models.TextField(max_length=512,
+    svn_repo_commit = models.TextField(
+        _("svn repo commit"),
+        max_length=512,
         blank=True,
         null=True
     )
-    svn_repo_username = models.CharField(max_length=256,
+    svn_repo_username = models.CharField(
+        _("svn repo username"),
+        max_length=256,
         blank=True,
         null=True
     )
-    svn_repo_password = models.TextField(max_length=256,
+    svn_repo_password = models.TextField(
+        _("svn repo password"),
+        max_length=256,
         blank=True,
         null=True
     )
 
     ## Project level permissions
-    allow_anon_viewing = models.BooleanField(default=False)
-    allow_anon_editing = models.BooleanField(default=False)
-    allow_anon_comment = models.BooleanField(default=False)
+    allow_anon_viewing = models.BooleanField(_("allow anon viewing"), default=False)
+    allow_anon_editing = models.BooleanField(_("allow anon editing"), default=False)
+    allow_anon_comment = models.BooleanField(_("allow anon comment"), default=False)
 
-    allow_authed_viewing = models.BooleanField(default=False)
-    allow_authed_editing = models.BooleanField(default=False)
-    allow_authed_comment = models.BooleanField(default=False)
+    allow_authed_viewing = models.BooleanField(_("allow authed viewing"), default=False)
+    allow_authed_editing = models.BooleanField(_("allow authed editing"), default=False)
+    allow_authed_comment = models.BooleanField(_("allow authed comment"), default=False)
 
     groups_can_view = models.ManyToManyField(Group,
         blank=True,
         null=True,
-        related_name='view_group_set'
+        related_name='view_group_set',
+        verbose_name=_("groups can view")
     )
     groups_can_edit = models.ManyToManyField(Group,
         blank=True,
         null=True,
-        related_name='edit_group_set'
+        related_name='edit_group_set',
+        verbose_name=_("groups can edit")
     )
     groups_can_comment = models.ManyToManyField(Group,
         blank=True,
         null=True,
-        related_name='comment_group_set'
+        related_name='comment_group_set',
+        verbose_name=_("groups can comment")
     )
 
     users_can_view = models.ManyToManyField(User,
         blank=True,
         null=True,
-        related_name='view_user_set'
+        related_name='view_user_set',
+        verbose_name=_("users can view")
     )
     users_can_edit = models.ManyToManyField(User,
         blank=True,
         null=True,
-        related_name='edit_user_set'
+        related_name='edit_user_set',
+        verbose_name=_("users can edit")
     )
     users_can_comment = models.ManyToManyField(User,
         blank=True,
         null=True,
-        related_name='comment_user_set'
+        related_name='comment_user_set',
+        verbose_name=_("users can comment")
     )
     watched_by = models.ManyToManyField(UserProfile,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("watched by")
+        
     )
 
-    active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(_("active"), default=True)
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -155,14 +178,14 @@ class Milestone(models.Model):
     """
     Goal to be reached
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
-    project = models.ForeignKey(Project)
-    description = models.TextField(blank=True, null=True)
-    goal_date = models.DateTimeField(blank=True, null=True)
-    active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
+    project = models.ForeignKey(Project, verbose_name=_("project"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    goal_date = models.DateTimeField(_("goal date"), blank=True, null=True)
+    active = models.BooleanField(_("active"), default=True)
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -179,12 +202,12 @@ class Component(models.Model):
     Components are items that are part of a project. For instance I have a
     project called Cars. We then have tires that are part of cars
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
-    project = models.ForeignKey(Project)
-    active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
+    project = models.ForeignKey(Project, verbose_name=_("project"))
+    active = models.BooleanField(_("active"), default=True)
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -201,12 +224,12 @@ class Version(models.Model):
     This will be FKed to projects. This will allow creation of issues
     that point at specific versions
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
-    project = models.ForeignKey(Project)
-    active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
+    project = models.ForeignKey(Project, verbose_name=_("project"))
+    active = models.BooleanField(_("active"), default=True)
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified_date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -222,8 +245,8 @@ class Status(models.Model):
     """
     Status dictate what status an issue is in, such as open, closed, etc
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
 
     def __unicode__(self):
         return self.name
@@ -237,8 +260,8 @@ class Priority(models.Model):
     """
     Priority dictates the emphasis an issue carries. Urgent, critical, etc
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
 
     def __unicode__(self):
         return self.name
@@ -255,8 +278,8 @@ class IssueType(models.Model):
     """
     Bug, defect, feature request, etc
     """
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(_("name"), max_length=256)
+    slug = models.SlugField(_("slug"), unique=True)
 
     def __unicode__(self):
         return self.name
@@ -271,44 +294,55 @@ class Issue(models.Model):
     This will be the root problem that is created. It will have a version,
     component, and so on.
     """
-    name = models.CharField(max_length=256)
-    project = models.ForeignKey(Project)
+    name = models.CharField(_("name"), max_length=256)
+    project = models.ForeignKey(Project, verbose_name=_("project"))
     component = models.ForeignKey(Component,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("component")
     )
     version = models.ForeignKey(Version,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("version")
     )
     milestone = models.ForeignKey(Milestone,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("milestone")
     )
-    status = models.ForeignKey(Status)
-    priority = models.ForeignKey(Priority)
+    status = models.ForeignKey(Status, 
+        verbose_name=_("status")
+    )
+    priority = models.ForeignKey(Priority,
+        verbose_name=_("priority")
+    )
     issue_type = models.ForeignKey(IssueType,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("issue type")
     )
-    description = models.TextField()
+    description = models.TextField(_("description"))
     created_by = models.ForeignKey(UserProfile,
         blank=True,
         null=True,
-        related_name="issue_creator"
+        related_name="issue_creator",
+        verbose_name=_("created by"),
     )
     assigned_to = models.ForeignKey(UserProfile,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("assigned to")
     )
     watched_by = models.ManyToManyField(UserProfile,
         blank=True,
         null=True,
-        related_name="watched_set"
+        related_name="watched_set",
+        verbose_name=_("watched by"),
     )
-    active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(_("active"), default=True)
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -325,11 +359,11 @@ class FileUpload(models.Model):
     File uploads will be FKed to specific issues. This will allow for 
     multiple files to be uploaded to a particular issue.
     """
-    name = models.CharField(max_length=256)
-    file = models.FileField(upload_to='attachments')
-    issue = models.ForeignKey(Issue)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(_("name"), max_length=256)
+    file = models.FileField(_("file"), upload_to='attachments')
+    issue = models.ForeignKey(Issue, verbose_name=_("issue"))
+    created_date = models.DateTimeField(_("created date"), auto_now_add=True)
+    modified_date = models.DateTimeField(_("modified date"), auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -345,35 +379,42 @@ class IssueFilter(models.Model):
     """
     This allows users to save their favorite view filters
     """
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, verbose_name=_("project"))
     user = models.ForeignKey(UserProfile,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("user")
     )
-    name = models.CharField(max_length=256)
+    name = models.CharField(_("name"), max_length=256)
     component = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("component"),
     )
     version = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("version")
     )
     milestone = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("milestone")
     )
     status = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("status")
     )
     type = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("type")
     )
     priority = models.CharField(max_length=256,
         blank=True,
-        null=True
+        null=True,
+        verbose_name=_("priority")
     )
 
     def __unicode__(self):
