65482ff4bbd1d7ba554dc515a20809decde2774d
= <=>
2009-12-21 19:58:01 -0600

    incremented the version after a few fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "djtracker",
-    version = "0.3.5",
+    version = "0.3.6",
     author = "Mark Rogers",
     author_email = "f4nt@f4ntasmic.com",
     url = "http://www.f4ntasmic.com",
